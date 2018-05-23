3448db8ded7e8ad376d429207e0c441f18c6586a
Bruno Correia <brunocs@msn.com>
2018-05-23 20:44:17 -0300

    Add JWT auth

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../../config/auth');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -27,5 +30,26 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// This hook runs before creating or updating users collection
+UserSchema.pre('save', async function hashPassword(next) {
+  // If there isn't a password change
+  if (!this.isModified('password')) next();
+
+  // Encrypt the password
+  this.password = await bcrypt.hash(this.password, 5);
+});
+
+UserSchema.methods = {
+  compareHash(password) {
+    return bcrypt.compare(password, this.password);
+  },
+
+  generateToken() {
+    return jwt.sign({ id: this.id }, authConfig.secret, {
+      expiresIn: 86400,
+    });
+  },
+};
 
 mongoose.model('User', UserSchema);
---
+++ config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: '8fc444b199070f81adb57bdc214b0514',
+};
