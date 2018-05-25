aa3a437e3439fa53ac632bad1c63e6eb5655f8c9
Bruno Correia <brunocs@msn.com>
2018-05-24 23:21:37 -0300

    Add JWT auth middleware

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,21 @@ const requireDir = require('require-dir');
 const routes = express.Router();
 
 const controllers = requireDir('./controllers');
+const authMiddleware = require('./middlewares/auth');
 
 /**
  * Auth
  */
 routes.post('/signup', controllers.authController.signup);
 routes.post('/signin', controllers.authController.signin);
+
+routes.use(authMiddleware);
+
+/**
+ * Authenticated routes
+ */
+routes.get('/tweets', (req, res) => {
+  res.send('You reached tweets.');
+});
 
 module.exports = routes;
