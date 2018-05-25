60386bd62d478021b2d6d59d031ea9cd6bd18a7d
Bruno Correia <brunocs@msn.com>
2018-05-25 12:01:21 -0300

    Add Sentry error tracking service

---
+++ config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  sentryDSN: 'https://bca22f42e0e34549be3d944fcd636aaa@sentry.io/1213693',
+};
--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
+const Raven = require('./app/services/sentry');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
+
+app.use(Raven.requestHandler());
+
 app.use('/api', require('./app/routes'));
+
+app.use(Raven.errorHandler());
 
 app.listen(3000);
