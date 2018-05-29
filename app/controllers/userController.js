b2e52f38e6e22b241d33a8c8186e96b2ec4a19dc
Bruno Correia <brunocs@msn.com>
2018-05-29 00:44:38 -0300

    Add like controller

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -12,7 +12,7 @@ module.exports = {
       } = req.body;
 
       if (password && password !== confirmPassword) {
-        return res.status(400).json({ error: "Password doesn't match" });
+        return res.status(400).json({ error: "Password doesn't match." });
       }
 
       const user = await User.findByIdAndUpdate(id, { name, username }, { new: true });
--- app/routes.js
+++ app/routes.js
@@ -32,4 +32,9 @@ routes.delete('/tweets/:id', controllers.tweetController.destroy);
  */
 routes.put('/users', controllers.userController.update);
 
+/**
+ * Likes
+ */
+routes.post('/like/:id', controllers.likeController.toggle);
+
 module.exports = routes;
