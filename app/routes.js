0156fc5e32fa739e2633c84750dab6d592ed95ae
Bruno Correia <brunocs@msn.com>
2018-05-28 22:11:15 -0300

    Add user controller

--- app/routes.js
+++ app/routes.js
@@ -27,4 +27,9 @@ routes.get('/tweets', (req, res) => {
 routes.post('/tweets', controllers.tweetController.create);
 routes.delete('/tweets/:id', controllers.tweetController.destroy);
 
+/**
+ * Users
+ */
+routes.put('/users', controllers.userController.update);
+
 module.exports = routes;
