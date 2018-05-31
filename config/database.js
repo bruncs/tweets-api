ccf5cb5a633559a595b432feadc277ecb4e30f27
Bruno Correia <brunocs@msn.com>
2018-05-31 02:45:50 -0300

    Add environment variables configuration

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  url: 'mongodb://localhost/tweets-api',
+  url: process.env.DATABASE_URL,
 
   modelsPath: path.resolve('app', 'models'),
 };
--- config/sentry.js
+++ config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  sentryDSN: 'https://bca22f42e0e34549be3d944fcd636aaa@sentry.io/1213693',
+  sentryDSN: process.env.SENTRY_DSN,
 };
